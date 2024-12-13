0f8347e0b3dbe4bf19147c97cb641889e556d76b
Phuoc Nguyen <nguyenvinhphuoc@outlook.com>
2024-12-13 20:33:12 +0700

    feat: implement job selection handling and update API calls for job data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
 import Header from "./Header";
+import { searchQueryStore } from "../stores/searchQueryStore";
+
+export function useSelectedJobItem() {
+  const setSelectedJobItem = searchQueryStore(
+    (state) => state.setSelectedJobItem
+  );
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setSelectedJobItem(+window.location.hash.slice(1));
+    };
+    handleHashChange();
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+}
 
 function App() {
+  useSelectedJobItem();
+
   return (
     <>
       <Background />
--- src/stores/searchQueryStore.ts
+++ src/stores/searchQueryStore.ts
@@ -1,21 +1,23 @@
 import { create } from "zustand";
 import { JobItem } from "../types";
+import { BASE_URL } from "../lib/constants";
 
 type Store = {
   jobItems: JobItem[];
   isLoading: boolean;
+  selectedJobItem: JobItem | undefined;
   searchRequest: (query: string) => Promise<void>;
+  setSelectedJobItem: (id: number) => void;
 };
 
 export const searchQueryStore = create<Store>((set) => ({
   jobItems: [],
   isLoading: false,
+  selectedJobItem: undefined,
   searchRequest: async (query: string) => {
     set(() => ({ isLoading: true }));
 
-    const response = await fetch(
-      `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${query}`
-    );
+    const response = await fetch(`${BASE_URL}?search=${query}`);
 
     if (!response.ok) {
       console.error("Failed to fetch job items");
@@ -24,5 +26,16 @@ export const searchQueryStore = create<Store>((set) => ({
 
     const data = await response.json();
     set(() => ({ jobItems: data.jobItems, isLoading: false }));
+  },
+  setSelectedJobItem: async (id: number) => {
+    const response = await fetch(`${BASE_URL}/${id}`);
+
+    if (!response.ok) {
+      console.error("Failed to fetch job items");
+      return;
+    }
+    const data = await response.json();
+    console.log(data);
+    set(() => ({ selectedJobItem: data.jobItem }));
   },
 }));
